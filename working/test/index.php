fb32896b4d002672b187e69c2a604f18b64068c6
ruslan <ruslan@explosion.ru>
2012-07-16 14:27:49 +0400

    Init (version 1.0)

--- working/test/index.php
+++ working/test/index.php
@@ -3,8 +3,8 @@
     require_once($_SERVER['DOCUMENT_ROOT'].'/api/core.class.php');
 
     //Класс текущего модуля
-    require_once('map.class.php');
+    require_once('test.class.php');
 
     //Запуск модуля
-    $core = new Map();
+    $core = new Test();
 ?>
